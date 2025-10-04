f585fae4d076d247bd30615e3fd01ce1254381a5
nal241 <cvxsr803a@g.ecc.u-tokyo.ac.jp>
2025-10-05 02:31:34 +0900

    clean: clang-format設定とVSCodeのC++/Cフォーマッタ修正

--- siv3d_bullet/PhysicsObject.h
+++ siv3d_bullet/PhysicsObject.h
@@ -6,63 +6,64 @@ class PhysicsWorld;
 
 struct BoxDesc
 {
-	s3d::Vec3 size;
-	s3d::Vec3 position;
-	float mass;
+    s3d::Vec3 size;
+    s3d::Vec3 position;
+    float mass;
 };
 
 struct SphereDesc
 {
-	float radius;
-	s3d::Vec3 position;
-	float mass;
+    float radius;
+    s3d::Vec3 position;
+    float mass;
 };
 
 enum class ShapeType
 {
-	Box,
-	Sphere,
+    Box,
+    Sphere,
 };
 
 class PhysicsObject
 {
-public:
-	~PhysicsObject();
+  public:
+    ~PhysicsObject();
 
-	// 削除禁止（moveのみ許可）
-	PhysicsObject(const PhysicsObject &) = delete;
-	PhysicsObject &operator=(const PhysicsObject &) = delete;
-	PhysicsObject(PhysicsObject &&) = default;
-	PhysicsObject &operator=(PhysicsObject &&) = default;
+    // 削除禁止（moveのみ許可）
+    PhysicsObject(const PhysicsObject&) = delete;
+    PhysicsObject& operator=(const PhysicsObject&) = delete;
+    PhysicsObject(PhysicsObject&&) = default;
+    PhysicsObject& operator=(PhysicsObject&&) = default;
 
-	PhysicsObject(PhysicsWorld *world, std::unique_ptr<btCollisionShape> shape, ShapeType type, float mass, const s3d::Vec3 &position);
+    PhysicsObject(PhysicsWorld* world, std::unique_ptr<btCollisionShape> shape, ShapeType type, float mass,
+                  const s3d::Vec3& position);
 
-	void update();
-	void draw();
-	void setPosition(const s3d::Vec3 &pos);
-	void setRotation(const s3d::Vec3 &rot);
-	void setRestitution(float restitution);
+    void update();
+    void draw();
+    void setPosition(const s3d::Vec3& pos);
+    void setRotation(const s3d::Vec3& rot);
+    void setRestitution(float restitution);
 
-	void setColor(const Color &color) { m_color = color; }
+    void setColor(const Color& color) { m_color = color; }
 
-	s3d::Vec3 getPosition() const;
-	s3d::Vec3 getRotation() const;
+    s3d::Vec3 getPosition() const;
+    s3d::Vec3 getRotation() const;
 
-	void applyForce(const s3d::Vec3 &force);
-	void applyImpulse(const s3d::Vec3 &impulse);
+    void applyForce(const s3d::Vec3& force);
+    void applyImpulse(const s3d::Vec3& impulse);
 
-	// btRigidBody *getRigidBody() const { return m_body; }
+    // btRigidBody *getRigidBody() const { return m_body; }
 
-private:
-	// Object は PhysicsWorldで管理する。
-	friend class PhysicsWorld;
+  private:
+    // Object は PhysicsWorldで管理する。
+    friend class PhysicsWorld;
 
-	std::unique_ptr<btCollisionShape> m_shape;
-	std::unique_ptr<btRigidBody> m_body;
-	std::unique_ptr<btMotionState> m_motionState;
+    std::unique_ptr<btCollisionShape> m_shape;
+    std::unique_ptr<btRigidBody> m_body;
+    std::unique_ptr<btMotionState> m_motionState;
 
-	PhysicsWorld *m_world = nullptr;
-	ShapeType m_shapeType;
-	// 描画
-	Color m_color = s3d::Linear::Palette::White;
+    PhysicsWorld* m_world = nullptr;
+    ShapeType m_shapeType;
+    // 描画
+    Color m_color = s3d::Linear::Palette::White;
 };
