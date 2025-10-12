732fb4be5a806a5e1eb0f94fb15f8e5813417dfc
nal241 <115138653+nal241@users.noreply.github.com>
2025-10-13 01:36:00 +0900

    clean: 射出コードのリファクタリング

--- siv3d_bullet/Player.h
+++ siv3d_bullet/Player.h
@@ -8,21 +8,30 @@ class Player
 public:
     Player(DebugCamera3D* camera, Model& coinModel);
 
-    enum class PlayerAction
-    {
-        ShootBox,
-        ShootSphere,
-        ShootCoin,
-    };
-
-    void executeAction(PlayerAction action);
-
     void handleInput(PhysicsWorld& world, Array<std::unique_ptr<PhysicsObject>>& objects);
     void setCamera(DebugCamera3D* camera);
 
 private:
+    struct Inputs
+    {
+        InputGroup shootBox{KeySpace};
+        InputGroup shootSphere{KeyO};
+        InputGroup shootCoin{KeyC};
+    };
+
+    enum class ObjectType
+    {
+        Box,
+        Sphere,
+        Coin,
+    };
+
+    void launchObject(ObjectType type, PhysicsWorld& world, Array<std::unique_ptr<PhysicsObject>>& objects);
+
     // 効果音ラボから音源は取得
     Audio m_shootSound{U"example/sounds/shoot.mp3"};
     DebugCamera3D* m_camera = nullptr;
     Model& m_coinModel;
+
+    Inputs m_inputs;
 };
