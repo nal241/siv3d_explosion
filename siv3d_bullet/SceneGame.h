021e1bf28ba59f8faf7f9fb203fc454ee438866d
nal241 <115138653+nal241@users.noreply.github.com>
2025-10-12 23:24:51 +0900

    update: Playerを追加

--- siv3d_bullet/SceneGame.h
+++ siv3d_bullet/SceneGame.h
@@ -2,6 +2,7 @@
 #include "SceneCommon.h"
 
 #include "PhysicsWorld.h"
+#include "Player.h"
 
 // ゲームシーン
 class SceneGame : public App::Scene
@@ -23,12 +24,8 @@ private:
     const MSRenderTexture m_renderTexture;
     DebugCamera3D m_camera;
 
+    Player m_player;
+
     Texture m_uvChecker{U"example/texture/uv.png", TextureDesc::MippedSRGB};
     Model m_model{U"model/coin.obj"};
-
-    // Audio
-    // 効果音ラボから音源は取得
-    Audio m_cubeShootSound{U"example/sounds/shoot.mp3"};
-    Audio m_sphereShootSound{U"example/sounds/shoot.mp3"};
-    double m_volume = 0.5;
 };
