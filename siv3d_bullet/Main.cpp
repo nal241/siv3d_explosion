c3b13a383fb08d22ea61f308f487c07a314def66
YusukeIzume <115138653+nal241@users.noreply.github.com>
2025-10-20 02:07:45 +0900

    add: タイトルとリザルトを追加

--- siv3d_bullet/Main.cpp
+++ siv3d_bullet/Main.cpp
@@ -1,17 +1,17 @@
 #include <Siv3D.hpp> // Siv3D v0.6.16
 
 #include "SceneCommon.h"
+#include "SceneTitle.h"
 #include "SceneGame.h"
+#include "SceneResult.h"
 #include "SceneTestExplosion.h"
 
 void Main()
 {
     App manager;
-    // manager.add<Title>(State::Title);
+    manager.add<SceneTitle>(State::Title);
     manager.add<SceneGame>(State::Game);
-    // manager.add<Ranking>(State::Ranking);
-
-    // 爆発用のシーン
+    manager.add<SceneResult>(State::Result);
     manager.add<SceneTestExplosion>(State::Explosion);
 
     // システムループ
---
+++ siv3d_bullet/SceneTitle.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "SceneCommon.h"
+
+// タイトルシーン
+class SceneTitle : public App::Scene
+{
+public:
+    SceneTitle(const InitData& init);
+
+    void update() override;
+
+    void draw() const override;
+
+private:
+    Font m_titleFont{60, Typeface::Bold};
+    Font m_menuFont{30};
+};
