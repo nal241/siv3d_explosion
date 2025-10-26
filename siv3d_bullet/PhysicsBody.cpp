03193f55bf7e2d000e588b0093f3fc483efb9aa4
nal241 <115138653+nal241@users.noreply.github.com>
2025-10-27 04:33:56 +0900

    fix: フラグのバグを修正

--- siv3d_bullet/PhysicsBody.cpp
+++ siv3d_bullet/PhysicsBody.cpp
@@ -92,16 +92,18 @@ void PhysicsBody::setCollisionCallback(std::function<void()> callback)
     m_collisionCallback = std::move(callback);
 
     // コールバックが設定されている場合、Bulletにカスタムマテリアルコールバックフラグを設定
+    int flags = m_body->getCollisionFlags();
     if (m_collisionCallback)
     {
         // このフラグにより、衝突判定時に独自の処理を実行できるようになる
-        m_body->setCollisionFlags(m_body->getCollisionFlags() | btCollisionObject::CF_CUSTOM_MATERIAL_CALLBACK);
+        flags |= btCollisionObject::CF_CUSTOM_MATERIAL_CALLBACK;
     }
     else
     {
         // コールバック解除
         flags &= ~btCollisionObject::CF_CUSTOM_MATERIAL_CALLBACK;
     }
+    m_body->setCollisionFlags(flags);
 }
 
 // 質量を取得
